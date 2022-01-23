1a11345b6587e62b5dbfcf9b960a736d82a83a60
Usama Ansari <usama251993@gmail.com>
2022-01-23 16:41:12 +0530

    ✔️ problem 2 solved

---
+++ puzzles/002-easy-unpack.ts
@@ -0,0 +1,36 @@
+/**
+ * ### Easy Unpack
+ *
+ *  Your mission here is to create a function that
+ * sets an `array` and returns a `tuple` with `3`
+ * elements - the first, third and second element
+ * from the last for the given array.
+ *
+ * ---
+ *
+ * **Input**: A tuple, at least 3 elements long.
+ *
+ * ---
+ *
+ * **Output**: A tuple.
+ *
+ * ---
+ *
+ * Example:
+ *
+ * ```ts
+ * easyUnpack([1, 2, 3, 4, 5, 6, 7, 9]) == [1, 3, 7]
+ * easyUnpack([1, 1, 1, 1]) == [1, 1, 1]
+ * easyUnpack([6, 3, 7]) == [6, 7, 3]
+ * ```
+ *
+ * @param {any[]} values Tuple of values
+ * @return {*}  {any[]} Tuple of values
+ */
+function easyUnpack(values: any[]): any[] {
+  return [
+    values[0],
+    values[2],
+    values[values.length - 2]
+  ];
+}
