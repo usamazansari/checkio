2523f5821a17079e249b2840f2fd34cee5de2125
Usama Ansari <usama251993@gmail.com>
2022-01-23 16:18:01 +0530

    🎉 init - repo
    - workspace setup
    - hot reload using nodemon
    - README.md
    - problem 1 solved

---
+++ puzzles/001-multiply-intro.ts
@@ -0,0 +1,34 @@
+/**
+ * ### Multiply Intro
+ *
+ * This is an intro mission, the purpose of which
+ * is to explain how to solve missions on CheckiO
+ * and how to get the most out of solving them. As
+ * you solve missions more stations become
+ * available to you, containing more complex
+ * missions.
+ *
+ * ---
+ *
+ * This mission is the easiest one. Write a
+ * function that will receive 2 numbers as input
+ * and it should return the multiplication of these
+ * 2 numbers.
+ *
+ * ---
+ *
+ * **Input**: Two arguments. Both are of type `int`.
+ *
+ * ---
+ *
+ * **Output**: `int`.
+ *
+ *
+ * @param a The first number
+ * @param b The second number
+ * @returns Product of `a` and `b`
+ */
+
+function multTwo(a: number, b: number): number {
+    return a * b;
+}
