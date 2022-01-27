c33e1a5ac043278faf56cd84da4319dab193d002
Usama Ansari <usama251993@gmail.com>
2022-01-27 22:08:02 +0530

    ✔️ problem home-#03 solved

---
+++ src/modules/home/03 First Word/first-word.puzzle.ts
@@ -0,0 +1,48 @@
+/**
+ * # First Word
+ * 
+ * - Difficulty: Elementary+
+ * 
+ * ## Description
+ * You are given a `string` where you have to find its first word.
+ * 
+ * When solving a task pay attention to the following points:
+ * - There can be `dot`s and `comma`s in a `string`.
+ * - A `string` can start with a letter or, for example, a dot `.` or
+ * space ` `.
+ * - A word can contain an apostrophe ` ` and it's a part of a word.
+ * - The whole text can be represented with one word and that's it.
+ * 
+ * ## Input
+ * A `string`.
+ * 
+ * ## Output
+ * A `string`.
+ * 
+ * ## Example
+ * ```ts
+ * firstWord("Hello world") == "Hello";
+ * firstWord("greetings, friends") == "greetings";
+ * ```
+ * 
+ * ## How it is used
+ * The first word is a command in a command line
+ * 
+ * ## Precondition
+ * The text can contain 
+ * ```
+ * a-z
+ * A-Z
+ * ,
+ * .
+ * '
+ * ```
+ *
+ * @param {string} text
+ * @return {*}  {string}
+ */
+function firstWord(text: string): string {
+  return text.match(/[A-Za-z\']*/g)?.filter(_ => !!_)[0] ?? '';
+}
+
+export { firstWord as my };
---
+++ src/modules/home/03 First Word/first-word.test.ts
@@ -0,0 +1,39 @@
+import { my } from './first-word.puzzle';
+
+const Tests = [
+  { text: 'Hello world', expected: 'Hello' },
+  { text: ' a word ', expected: 'a' },
+  { text: 'don\'t touch it', expected: 'don\'t' },
+  { text: 'greetings, friends', expected: 'greetings' },
+  { text: '... and so on ...', expected: 'and' },
+  { text: 'hi', expected: 'hi' },
+  { text: 'Holy Edison', expected: 'Holy' },
+  { text: 'Don\'t speak... I know just what you\'re saying', expected: 'Don\'t' },
+  { text: 'Hello.World', expected: 'Hello' }
+];
+
+describe('Easy Unpack using', () => {
+  describe('My Solution', () => {
+    Tests.forEach(test => {
+      it(`should return the correct result for ${test.text}`, () => {
+        expect(my(test.text)).toBe(test.expected);
+      });
+    });
+  });
+
+  // describe('Best solution', () => {
+  //   Tests.forEach(test => {
+  //     it(`should return the correct result for ${test.text}`, () => {
+  //       expect(best(test.text)).toBe(test.expected);
+  //     });
+  //   });
+  // });
+
+  // describe('Creative solution', () => {
+  //   Tests.forEach(test => {
+  //     it(`should return the correct result for ${test.text}`, () => {
+  //       expect(creative(test.text)).toBe(test.expected);
+  //     });
+  //   });
+  // });
+});
