9530dc2a719623f0f650f7487989000c6b6fa807
Usama Ansari <usama251993@gmail.com>
2022-01-27 19:46:48 +0530

    ✔️ problem #015 solved

---
+++ src/modules/initiation/15 Between Markers Simplified/between-markers-simplified.puzzle.ts
@@ -0,0 +1,50 @@
+/**
+ * # Between Markers Simplified
+ * 
+ * - Difficulty: Elementary
+ * 
+ * ## Description
+ * You are given a `string` and two markers (the initial one and final).
+ * You have to find a `substring` enclosed between these two markers.
+ * But there are a few important conditions.
+ * 
+ * > This is a simplified version of the 
+ * [Between Markers](https://js.checkio.org/mission/between-markers/) 
+ * mission.
+ * 
+ * - The initial and final markers are always different.
+ * - The initial and final markers are always 1 char size.
+ * - The initial and final markers always exist in a string and go one after another.
+ * 
+ * ##Input
+ * - Three arguments.
+ * - All of them are `string`s.
+ * - The second and third arguments are the initial and final markers.
+ * 
+ * ## Output
+ * A `string`.
+ * 
+ * ## Example
+ * ```ts
+ * betweenMarkers('What is >apple<', '>', '<') == 'apple';
+ * betweenMarkers('What is [apple]', '[', ']') == 'apple';
+ * betweenMarkers('What is ><', '>', '<') == '';
+ * betweenMarkers('[an apple]', '[', ']') == 'an apple';
+ * ```
+ * 
+ * ## How it is used
+ * For text parsing.
+ * 
+ * ## Precondition
+ * There can't be more than one final and one initial markers.
+ *
+ * @param {string} line
+ * @param {string} left
+ * @param {string} right
+ * @return {*}  {string}
+ */
+function betweenMarkers(line: string, left: string, right: string): string {
+  return (line.split(left)[1] || '').split(right)[0] || '';
+}
+
+export { betweenMarkers as my };
---
+++ src/modules/initiation/15 Between Markers Simplified/between-markers-simplified.test.ts
@@ -0,0 +1,35 @@
+import { my } from './between-markers-simplified.puzzle';
+
+const Tests = [
+  { line: 'What is >apple<', left: '>', right: '<', expected: 'apple' },
+  { line: 'What is [apple]', left: '[', right: ']', expected: 'apple' },
+  { line: 'What is ><', left: '>', right: '<', expected: '' },
+  { line: '[an apple]', left: '[', right: ']', expected: 'an apple' },
+  { line: '>Apple< is simple', left: '>', right: '<', expected: 'an apple' }
+];
+
+describe('Between Markers Simplified using', () => {
+  describe('My Solution', () => {
+    Tests.forEach(test => {
+      it(`should return the correct result for searching between '${test.left}' and '${test.right}' in '${test.line}'`, () => {
+        expect(my(test.line, test.left, test.right)).toBe(test.expected);
+      });
+    });
+  });
+
+  // describe('Best solution', () => {
+  //   Tests.forEach(test => {
+  //     it(`should return the correct result for searching ${test.search} in ${test.values}`, () => {
+  //       expect(best(test.values, test.search)).toBe(test.expected);
+  //     });
+  //   });
+  // });
+
+  // describe('Creative solution', () => {
+  //   Tests.forEach(test => {
+  //     it(`should return the correct result for searching ${test.search} in ${test.values}`, () => {
+  //       expect(creative(test.values, test.search)).toBe(test.expected);
+  //     });
+  //   });
+  // });
+});
