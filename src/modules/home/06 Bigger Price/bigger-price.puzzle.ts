55166beaa140e53dc0b70bb84a541639466df6d1
Usama Ansari <usama251993@gmail.com>
2022-01-29 11:28:49 +0530

    ✔️ home-#06 solved

---
+++ src/modules/home/06 Bigger Price/bigger-price.puzzle.ts
@@ -0,0 +1,55 @@
+/**
+ * Stock interface
+ *
+ * @interface Stock
+ */
+interface Stock {
+  name: string,
+  price: number,
+};
+
+/**
+ * # Bigger Price
+ * 
+ * - Difficulty: Elementary+
+ * 
+ * ## Description
+ * You have a table with all available goods in the store. The data is 
+ * represented as a list of `objects`
+ * 
+ * Your mission here is to find the **TOP** most expensive goods. The 
+ * amount we are looking for will be given as a first argument and the 
+ * whole data as the second one
+ * 
+ * ## Input
+ * - `Integer` and `array` of `object`s.
+ * - Each `object` has two attributes `name` and `price`
+ * 
+ * ## Output
+ * the same as the second Input argument.
+ * 
+ * Example
+ * ```ts
+ * biggerPrice(2, [
+ *     {"name": "bread", "price": 100},
+ *     {"name": "wine", "price": 138},
+ *     {"name": "meat", "price": 15},
+ *     {"name": "water", "price": 1}
+ * ]) == [
+ *     {"name": "wine", "price": 138},
+ *     {"name": "bread", "price": 100}
+ * ];
+ * biggerPrice(1, [
+ *     {"name": "pen", "price": 5},
+ *     {"name": "whiteboard", "price": 170}
+ * ]) == [{"name": "whiteboard", "price": 170}];
+ * 
+ * @param {number} limit
+ * @param {Stock[]} data
+ * @return {*}  {Stock[]}
+ */
+function biggerPrice(limit: number, data: Stock[]): Stock[] {
+  return data.sort((a, b) => b.price - a.price).slice(0, limit);
+}
+
+export { biggerPrice as my };
---
+++ src/modules/home/06 Bigger Price/bigger-price.test.ts
@@ -0,0 +1,64 @@
+import { my } from './bigger-price.puzzle';
+
+const Tests = [
+  {
+    limit: 2,
+    data: [
+      { "name": "bread", "price": 100 },
+      { "name": "wine", "price": 138 },
+      { "name": "meat", "price": 15 },
+      { "name": "water", "price": 1 }
+    ],
+    expected: [
+      { "name": "wine", "price": 138 },
+      { "name": "bread", "price": 100 }
+    ]
+  },
+  {
+    limit: 1,
+    data: [
+      { "name": "pen", "price": 5 },
+      { "name": "whiteboard", "price": 170 }
+    ],
+    expected: [{ "name": "whiteboard", "price": 170 }]
+  },
+  {
+    limit: 2,
+    data: [
+      { "name": "bread", "price": 10 },
+      { "name": "wine", "price": 138 },
+      { "name": "meat", "price": 15 },
+      { "name": "milk", "price": 25 }
+    ],
+    expected: [
+      { "name": "wine", "price": 138 },
+      { "name": "milk", "price": 25 }
+    ]
+  }
+];
+
+describe('Bigger Price using', () => {
+  describe('My Solution', () => {
+    Tests.forEach(test => {
+      it(`should return '${test.expected}' for '${test.limit}'`, () => {
+        expect(my(test.limit, test.data)).toEqual(test.expected);
+      });
+    });
+  });
+
+  // describe('Best solution', () => {
+  //   Tests.forEach(test => {
+  //     it(`should return '${test.expected}' for '${test.text}'`, () => {
+  //       expect(best(test.text)).toEqual(test.expected);
+  //     });
+  //   });
+  // });
+
+  // describe('Creative solution', () => {
+  //   Tests.forEach(test => {
+  //     it(`should return '${test.expected}' for '${test.text}'`, () => {
+  //       expect(creative(test.text)).toEqual(test.expected);
+  //     });
+  //   });
+  // });
+});
