6b6fa2d9963edfb304d9f0ec00df29c1624cd02a
Usama Ansari <usamazansari@outlook.com>
2022-02-06 17:07:06 +0530

    ✨ inititation-#06 solutions

--- src/modules/initiation/06 End Zeros/end-zeros.test.ts
+++ src/modules/initiation/06 End Zeros/end-zeros.test.ts
@@ -1,38 +1,58 @@
-import { my, best, creative } from './end-zeros.puzzle';
+import { Setup, Tester } from '../../../common';
+import { Puzzle } from './end-zeros.puzzle';
 
-const Tests = [
-  { value: 0, expected: 1 },
-  { value: 1, expected: 0 },
-  { value: 10, expected: 1 },
-  { value: 101, expected: 0 },
-  { value: 245, expected: 0 },
-  { value: 100100, expected: 2 },
-  { value: 3456, expected: 0 },
-  { value: 100234, expected: 0 }
-];
-
-describe('End Zeros using', () => {
-  describe('My Solution', () => {
-    Tests.forEach(test => {
-      it(`should return '${test.expected}' for '${test.value}'`, () => {
-        expect(my(test.value)).toBe(test.expected);
-      });
-    });
-  });
-
-  describe('Best solution', () => {
-    Tests.forEach(test => {
-      it(`should return '${test.expected}' for '${test.value}'`, () => {
-        expect(best(test.value)).toBe(test.expected);
-      });
-    });
-  });
-
-  describe('Creative solution', () => {
-    Tests.forEach(test => {
-      it(`should return '${test.expected}' for '${test.value}'`, () => {
-        expect(creative(test.value)).toBe(test.expected);
-      });
-    });
-  });
+new Tester({
+  solution: Puzzle,
+  tests: new Setup({
+    tests: [
+      {
+        arguments: [
+          { value: 0 }
+        ],
+        expected: 1
+      },
+      {
+        arguments: [
+          { value: 1 }
+        ],
+        expected: 0
+      },
+      {
+        arguments: [
+          { value: 10 }
+        ],
+        expected: 1
+      },
+      {
+        arguments: [
+          { value: 101 }
+        ],
+        expected: 0
+      },
+      {
+        arguments: [
+          { value: 245 }
+        ],
+        expected: 0
+      },
+      {
+        arguments: [
+          { value: 100100 }
+        ],
+        expected: 2
+      },
+      {
+        arguments: [
+          { value: 3456 }
+        ],
+        expected: 0
+      },
+      {
+        arguments: [
+          { value: 100234 }
+        ],
+        expected: 0
+      }
+    ]
+  })
 });
