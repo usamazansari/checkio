18ea92e15fa6c4dd8b23ea7ac57e363e3158dcb8
Usama Ansari <usamazansari@outlook.com>
2022-02-06 13:43:21 +0530

    ⚡optimized deepEquality

--- src/common/models/tester.ts
+++ src/common/models/tester.ts
@@ -5,17 +5,17 @@ export class Tester {
     {
       solution = new Solution({}),
       tests = new Setup({}),
-      deepEquality = false
     }:
-      Partial<{ solution: Solution; tests: Setup; deepEquality?: boolean; }>
+      Partial<{ solution: Solution; tests: Setup; }>
   ) {
+    const deepEquality = typeof (tests[0].expected) === 'object';
     describe(`${solution.name} using`, () => {
       solution.categories.forEach(category => {
         describe(`${category.name} Solution`, () => {
           tests.forEach(test => {
             it(`should return ${this.formatAssertion(test.expected)} for ${test.arguments.map((_: any) => this.formatAssertion(_)).join(', ')}`, () => {
               deepEquality
-                ? expect(category.fn(...test.arguments)).toEqual(test.expected)
+                ? expect(category.fn(...test.arguments)).toStrictEqual(test.expected)
                 : expect(category.fn(...test.arguments)).toBe(test.expected);
             });
           });
--- src/modules/initiation/02 Easy Unpack/easy-unpack.test.ts
+++ src/modules/initiation/02 Easy Unpack/easy-unpack.test.ts
@@ -39,6 +39,5 @@ new Tester(
           }
         ]
       }
-    ),
-    deepEquality: true
+    )
   });
