258201ab8bea2ce49e2318c7b7f324dc3b93bef2
Usama Ansari <usamazansari@outlook.com>
2022-02-06 19:01:02 +0530

    ✨ initiation-#12 solutions

--- src/modules/initiation/12 Split Pairs/split-pairs.puzzle.ts
+++ src/modules/initiation/12 Split Pairs/split-pairs.puzzle.ts
@@ -1,3 +1,5 @@
+import { Solution } from '../../../common';
+
 /**
  * # Split Pairs
  * 
@@ -40,10 +42,58 @@ function splitPairs(text: string): string[] {
   return output;
 }
 
-const best = (t: string) => (t + '_').match(/../g) || [];
-
-const creative = (s: string) => s.padEnd(s.length + s.length % 2, '_')
-  .split(/(\w{2})/)
-  .filter(n => !!n);
-
-export { splitPairs as my, best, creative };
+export const Puzzle: Solution = new Solution({
+  name: 'Split Pairs',
+  categories: [
+    {
+      name: 'My',
+      fn: splitPairs,
+    },
+    {
+      name: 'Clear',
+      fn: (t: string) => (t + '_').match(/../g) || []
+    },
+    {
+      name: 'Creative',
+      fn: (s: string) => s.padEnd(s.length + s.length % 2, '_')
+        .split(/(\w{2})/)
+        .filter(n => !!n)
+    },
+    {
+      name: 'Third Party',
+      fn(text: string): string[] {
+        if (text.length < 0 || text.length > 100) return ['0<=len(str)<=100'];
+        var tabStr = text.split('');
+        var tabRes = [];
+        var pairCount = 0;
+        var pairConstr: string = '';
+        tabStr.map(t => {
+          if (pairCount == 1) {
+            pairConstr = pairConstr + t;
+            tabRes.push(pairConstr);
+            pairCount = 0;
+          } else {
+            pairConstr = t;
+            pairCount++;
+          }
+        });
+        if (pairCount == 1) {
+          pairConstr = pairConstr + '_';
+          tabRes.push(pairConstr);
+        }
+        return tabRes;
+      },
+    },
+    {
+      name: 'Uncategorized',
+      fn(text: string): string[] {
+        var a = text.length % 2 != 0 ? text.concat('_') : text;
+        return a.length > 0 ? a.match(/.{1,2}/g)?.filter(_ => !!_) ?? [] : [];
+      },
+    },
+    {
+      name: 'Github Copilot',
+      fn(t: string): string[] { return `${t}_`.match(/.{2}/g) || []; }
+    }
+  ]
+});
--- src/modules/initiation/12 Split Pairs/split-pairs.test.ts
+++ src/modules/initiation/12 Split Pairs/split-pairs.test.ts
@@ -1,37 +1,52 @@
-import { my, best, creative } from './split-pairs.puzzle';
+import { Setup, Tester } from '../../../common';
+import { Puzzle } from './split-pairs.puzzle';
 
-const Tests = [
-  { text: 'abcd', expected: ['ab', 'cd'] },
-  { text: 'abc', expected: ['ab', 'c_'] },
-  { text: 'abcdf', expected: ['ab', 'cd', 'f_'] },
-  { text: 'a', expected: ['a_'] },
-  { text: '', expected: [] },
-  { text: 'hhh', expected: ['hh', 'h_'] },
-  { text: 'hhh1', expected: ['hh', 'h1'] }
-];
-
-describe('Split Pairs using', () => {
-  describe('My Solution', () => {
-    Tests.forEach(test => {
-      it(`should return '${test.expected}' for '${test.text}'`, () => {
-        expect(my(test.text)).toEqual(test.expected);
-      });
-    });
-  });
-
-  describe('Best solution', () => {
-    Tests.forEach(test => {
-      it(`should return '${test.expected}' for '${test.text}'`, () => {
-        expect(best(test.text)).toEqual(test.expected);
-      });
-    });
-  });
-
-  describe('Creative solution', () => {
-    Tests.forEach(test => {
-      it(`should return '${test.expected}' for '${test.text}'`, () => {
-        expect(creative(test.text)).toEqual(test.expected);
-      });
-    });
-  });
+new Tester({
+  solution: Puzzle,
+  tests: new Setup({
+    tests: [
+      {
+        arguments: [
+          { text: 'abcd' }
+        ],
+        expected: ['ab', 'cd']
+      },
+      {
+        arguments: [
+          { text: 'abc' }
+        ],
+        expected: ['ab', 'c_']
+      },
+      {
+        arguments: [
+          { text: 'abcdf' }
+        ],
+        expected: ['ab', 'cd', 'f_']
+      },
+      {
+        arguments: [
+          { text: 'a' }
+        ],
+        expected: ['a_']
+      },
+      {
+        arguments: [
+          { text: '' }
+        ],
+        expected: []
+      },
+      {
+        arguments: [
+          { text: 'hhh' }
+        ],
+        expected: ['hh', 'h_']
+      },
+      {
+        arguments: [
+          { text: 'hhh1' }
+        ],
+        expected: ['hh', 'h1']
+      }
+    ],
+  }),
 });
