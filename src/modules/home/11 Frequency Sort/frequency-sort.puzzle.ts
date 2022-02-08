73def3d494e05654e79bffc26065c39a58aea52f
Usama Ansari <usamazansari@outlook.com>
2022-02-08 19:48:26 +0530

    ✨ home-#11 solutions

--- src/modules/home/11 Frequency Sort/frequency-sort.puzzle.ts
+++ src/modules/home/11 Frequency Sort/frequency-sort.puzzle.ts
@@ -1,3 +1,5 @@
+import { Solution } from '../../../common';
+
 interface SortDict {
   item: string | number;
   index: number;
@@ -56,4 +58,99 @@ function frequencySort(items: (string | number)[]): (string | number)[] {
   return output;
 }
 
-export { frequencySort as my };
+export const Puzzle = new Solution({
+  name: 'Sort Array by Element Frequency',
+  categories: [
+    {
+      name: 'My',
+      fn: frequencySort
+    },
+    {
+      name: 'Clear',
+      fn(items: any[]): any[] {
+        let count: any = {};
+        for (const item of items) {
+          count[item] = count[item] ? count[item] + 1 : 1;
+        }
+        return items.sort((a, b) => count[a] != count[b]
+          ? count[b] - count[a]
+          : items.indexOf(a) - items.indexOf(b));
+      }
+    },
+    {
+      name: 'Creative',
+      fn(items: any[]): any[] {
+        function count(value: any): number {
+          let count = 0;
+          for (let item of items) {
+            if (item === value) count++;
+          }
+          return count;
+        }
+
+        function compare(a: any, b: any): number {
+          let diff = count(b) - count(a);
+          return !!diff ? diff : items.indexOf(a) - items.indexOf(b);
+        }
+
+        items.sort(compare);
+        return items;
+      }
+    },
+    {
+      name: 'Third Party',
+      fn(items: any[]): any[] {
+        // a monster solution
+        items.length == 0
+          ? []
+          : items;
+        let plain = items.slice();
+        plain = plain.some((x: any) => typeof x == 'string')
+          ? plain.sort()
+          : plain.sort((a: number, b: number): number => a - b);
+
+        let numbers = [];
+
+        for (let i = 0; i < plain.length; i++) {
+          let z = plain.slice(plain.indexOf(plain[i]), plain.lastIndexOf(plain[i]) + 1);
+          numbers.push(z);
+          i = i + z.length - 1;
+        }
+
+        let sorted = numbers.sort((a, b) => b.length - a.length);
+
+        for (let i = 0; i < sorted.length; i++) {
+          for (let k = i + 1; k < sorted.length; k++) {
+
+            if (sorted[i].length == sorted[k].length) {
+
+              if (items.indexOf(sorted[i][0]) > items.indexOf(sorted[k][0])) {
+                let temp = sorted[i];
+                sorted[i] = sorted[k];
+                sorted[k] = temp;
+
+              }
+            }
+          }
+        }
+
+        return items.length == 0
+          ? []
+          : plain.some((x: any) => typeof x == 'string')
+            ? sorted.join(',').split(',')
+            : sorted.join(',').split(',').map((x) => Number(x));
+
+      }
+    },
+    {
+      name: 'Uncategorized',
+      fn(items: any[]): any[] {
+        function count(list: any[], item: any): any {
+          return list.filter((x: any) => x == item).length;
+        }
+        return items.sort((a, b) => items.indexOf(a) - items.indexOf(b))
+          .sort((a, b) => count(items, b) - count(items, a));
+      }
+    }
+  ]
+});
--- src/modules/home/11 Frequency Sort/frequency-sort.test.ts
+++ src/modules/home/11 Frequency Sort/frequency-sort.test.ts
@@ -1,38 +1,58 @@
-import { my } from './frequency-sort.puzzle';
+import { Setup, Tester } from '../../../common';
+import { Puzzle } from './frequency-sort.puzzle';
 
-const Tests = [
-  { items: [4, 6, 2, 2, 6, 4, 4, 4], expected: [4, 4, 4, 4, 6, 6, 2, 2] },
-  { items: ['bob', 'bob', 'carl', 'alex', 'bob'], expected: ['bob', 'bob', 'bob', 'carl', 'alex'] },
-  { items: [17, 99, 42], expected: [17, 99, 42] },
-  { items: [], expected: [] },
-  { items: [1], expected: [1] },
-  { items: [6, 3], expected: [6, 3] },
-  { items: [1, 1, 1, 1], expected: [1, 1, 1, 1] },
-  { items: [1, 2, 2, 1], expected: [1, 1, 2, 2] }
-];
-
-describe('Frequency Sort using', () => {
-  describe('My Solution', () => {
-    Tests.forEach(test => {
-      it(`should return '${test.expected}' for '${test.items}'`, () => {
-        expect(my(test.items)).toEqual(test.expected);
-      });
-    });
-  });
-
-  // describe('Best solution', () => {
-  //   Tests.forEach(test => {
-  //     it(`should return '${test.expected}' for '${test.items}'`, () => {
-  //       expect(best(test.items)).toEqual(test.expected);
-  //     });
-  //   });
-  // });
-
-  // describe('Creative solution', () => {
-  //   Tests.forEach(test => {
-  //     it(`should return '${test.expected}' for '${test.items}'`, () => {
-  //       expect(creative(test.items)).toEqual(test.expected);
-  //     });
-  //   });
-  // });
-});
+new Tester({
+  solution: Puzzle,
+  setup: new Setup({
+    tests: [
+      {
+        arguments: [
+          { items: [4, 6, 2, 2, 6, 4, 4, 4] }
+        ],
+        expected: [4, 4, 4, 4, 6, 6, 2, 2]
+      },
+      {
+        arguments: [
+          { items: ['bob', 'bob', 'carl', 'alex', 'bob'] }
+        ],
+        expected: ['bob', 'bob', 'bob', 'carl', 'alex']
+      },
+      {
+        arguments: [
+          { items: [17, 99, 42] }
+        ],
+        expected: [17, 99, 42]
+      },
+      {
+        arguments: [
+          { items: [] }
+        ],
+        expected: []
+      },
+      {
+        arguments: [
+          { items: [1] }
+        ],
+        expected: [1]
+      },
+      {
+        arguments: [
+          { items: [6, 3] }
+        ],
+        expected: [6, 3]
+      },
+      {
+        arguments: [
+          { items: [1, 1, 1, 1] }
+        ],
+        expected: [1, 1, 1, 1]
+      },
+      {
+        arguments: [
+          { items: [1, 2, 2, 1] }
+        ],
+        expected: [1, 1, 2, 2]
+      }
+    ]
+  }),
+}).run();
